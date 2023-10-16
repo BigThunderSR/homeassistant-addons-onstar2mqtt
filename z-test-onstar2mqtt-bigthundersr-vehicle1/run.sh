39fbd9a4efe4a7497477e5ef68cbd1de2191fd19
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2023-10-16 13:03:30 -0500

    Testing

--- z-test-onstar2mqtt-bigthundersr-vehicle1/run.sh
+++ z-test-onstar2mqtt-bigthundersr-vehicle1/run.sh
@@ -16,7 +16,7 @@ export MQTT_USERNAME=$(bashio::config 'MQTT_USERNAME')
 export MQTT_PASSWORD=$(bashio::config 'MQTT_PASSWORD')
 export MQTT_PORT=$(bashio::config 'MQTT_PORT')
 #export MQTT_TLS=$(bashio::config 'MQTT_TLS')
-#export MQTT_PREFIX=$(bashio::config 'MQTT_PREFIX')
+export MQTT_PREFIX=$(bashio::config 'MQTT_PREFIX')
 
 bashio::log.info "Starting OnStar2MQTT for $VEHICLE_NAME..."
 npm run start
