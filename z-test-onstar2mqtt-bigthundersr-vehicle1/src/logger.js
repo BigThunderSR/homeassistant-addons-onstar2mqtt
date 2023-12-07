d6c9f4039efdf50d9a12121301355ab80b0c71c7
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2023-12-07 15:10:27 -0600

    Testing

--- z-test-onstar2mqtt-bigthundersr-vehicle1/src/logger.js
+++ z-test-onstar2mqtt-bigthundersr-vehicle1/src/logger.js
@@ -4,12 +4,18 @@ const _ = require('lodash');
 const logger = winston.createLogger({
     level: _.get(process, 'env.LOG_LEVEL', 'info'),
     format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf((info) => {
+          const {
+            timestamp, level, message, ...args
+          } = info;
+    
+          const ts = timestamp.slice(0, 19).replace('T', ' ');
+          return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
         }),
-        winston.format.colorize({ all: true }),
-        winston.format.simple()
-    ),
+          ),
     // format: winston.format.json(),
     transports: [new winston.transports.Console({stderrLevels: ['error']})]
 })
