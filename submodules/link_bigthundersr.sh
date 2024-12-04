ad0a94cf766b0ba3c9a5dfa363792f46c7d8197f
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-12-04 16:32:08 -0600

    Updates

--- submodules/link_bigthundersr.sh
+++ submodules/link_bigthundersr.sh
@@ -38,4 +38,42 @@ ln -f ../submodules/bigthundersr/onstar2mqtt/src/deps/index.cjs ./src/deps/index
 
 ln -f ../submodules/bigthundersr/onstar2mqtt/HA-MQTT.md ./DOCS.md
 
+echo "Vehicle 3"
+
+cd ../onstar2mqtt-bigthundersr-vehicle3/
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/package.json package.json
+ln -f ../submodules/bigthundersr/onstar2mqtt/package-lock.json package-lock.json
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/commands.js ./src/commands.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/diagnostic.js ./src/diagnostic.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/index.js ./src/index.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/logger.js ./src/logger.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/measurement.js ./src/measurement.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/mqtt.js ./src/mqtt.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/vehicle.js ./src/vehicle.js
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/deps/index.cjs ./src/deps/index.cjs
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/HA-MQTT.md ./DOCS.md
+
+echo "Vehicle 4"
+
+cd ../onstar2mqtt-bigthundersr-vehicle4/
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/package.json package.json
+ln -f ../submodules/bigthundersr/onstar2mqtt/package-lock.json package-lock.json
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/commands.js ./src/commands.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/diagnostic.js ./src/diagnostic.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/index.js ./src/index.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/logger.js ./src/logger.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/measurement.js ./src/measurement.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/mqtt.js ./src/mqtt.js
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/vehicle.js ./src/vehicle.js
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/src/deps/index.cjs ./src/deps/index.cjs
+
+ln -f ../submodules/bigthundersr/onstar2mqtt/HA-MQTT.md ./DOCS.md
+
 echo "Done!"
